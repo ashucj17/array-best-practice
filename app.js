399a8ec36f064307d43934a2c74ee155c8ec8f4c
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-10-02 18:05:23 +0530

    splice method solve

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ let shoppingList = [
   ["apples", "bananas", "oranges"],
   ["potatos", "tomatos", "onions"],
 ];
-console.log("before splice", shoppingList);
 // console.log(shoppingList[0]);
 // shoppingList[4].push("carrot");
 // console.log(shoppi4ngList);
 // shoppingList.pop();
-// console.log("before splice", shoppingList);
+console.log("before splice", shoppingList);
+// console.log(shoppingList[4]);
 shoppingList[4].splice(1, 2, "Cucumbers", "Bell Peppers");
 console.log("after splice", shoppingList);
